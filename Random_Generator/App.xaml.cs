22a14d0ef1c6a4181be18cb41f28f17b11f37b38
埃博拉酱 <vhtmfu@outlook.com>
2023-02-04 15:35:13 +0800

    v2.0.1 窗口位置和尺寸恢复上次 保存临时证书、商店关联到GitHub 分离商店和旁加载配制

--- Random_Generator/App.xaml.cs
+++ Random_Generator/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Random_Generator;
+﻿using Microsoft.Maui.Platform;
+
+namespace Random_Generator;
 
 public partial class App : Application
 {
@@ -8,4 +10,28 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+#if WINDOWS
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		Window 窗口 = base.CreateWindow(activationState);
+		窗口.Height = Preferences.Default.Get("窗口高度", double.NaN);
+		窗口.Width = Preferences.Default.Get("窗口宽度", double.NaN);
+		窗口.X = Preferences.Default.Get("窗口X", double.NaN);
+		窗口.Y = Preferences.Default.Get("窗口Y", double.NaN);
+		窗口.SizeChanged += 窗口_SizeChanged;
+		return 窗口;
+	}
+
+	private void 窗口_SizeChanged(object sender, EventArgs e)
+	{
+		Window 窗口 = sender as Window;
+		if (窗口.Y > 0)
+		{
+			Preferences.Default.Set("窗口高度", 窗口.Height);
+			Preferences.Default.Set("窗口宽度", 窗口.Width);
+			Preferences.Default.Set("窗口X", 窗口.X);
+			Preferences.Default.Set("窗口Y", 窗口.Y);
+		}
+	}
+#endif
 }
